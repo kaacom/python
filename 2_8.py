2919ab82465c91d099ef6aafac5bb9e4c8d4a185
akubarev <alexeykubarev@gmail.com>
2022-04-03 02:48:47 +0300

    test

--- 2_8.py
+++ 2_8.py
@@ -1,3 +1,4 @@
+# test 03/04-2:45
 f_name = input()
 f = open(f_name, 'r')
 all_line = f.read()
@@ -6,5 +7,6 @@ parts_1 = filter(None, parts)
 numbers = [int(P) for P in list(parts_1)]
 summ_num = sum(numbers)
 print(summ_num)
+
 
 f.close()
